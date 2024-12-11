3609dea1061d3ee00ec2b86e3af530d3d103ca11
AkshayKarthickMS <117292543+AkshayKarthickMS@users.noreply.github.com>
2024-12-12 04:22:13 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  port: parseInt(process.env.PORT) || 4173, // Use Render's PORT environment variable
-  host: true, 
-})
+  preview: {
+    port: parseInt(process.env.PORT) || 4173, // Use Render's PORT environment variable
+    host: true, // Expose to the network
+  },
+});
