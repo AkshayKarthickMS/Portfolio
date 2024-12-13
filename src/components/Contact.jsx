9252a16c1e0611e781f0cbe3b9b47044970f531c
AkshayKarthickMS <117292543+AkshayKarthickMS@users.noreply.github.com>
2024-12-13 21:16:58 +0530

    Update Contact.jsx

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -84,7 +84,7 @@ const Contact = () => {
       className='w-10 h-10 flex items-center justify-center rounded-full shadow-md'
     >
       <img
-        src='/src/assets/github.png'
+        src='src/assets/github.png'
         alt='GitHub'
         className='w-10 h-10'
       />
